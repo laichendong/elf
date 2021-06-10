1d88d753b0af9dccf01e3084a6b8bfbbf740a6b2
laichendong <laichendong@ZBMAC-C02VR1VYHTD5.local>
2021-06-10 20:28:20 +0800

    s

--- c/changedWord.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[] ){
-    printf("%s   %s   %s", argv[0], argv[1], argv[2]);
-    if (argc == 2) {
-        printf("%s   %s", argv[0], argv[1]);
-    }
-}
